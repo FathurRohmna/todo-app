56de5157be005aa4ebe4b6c117a797fb1bde1777
Fathur Rohman <fr081938@gmail.com>
2025-04-06 14:23:57 +0700

    feat: items and category model

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 class RegistrationView(View):
@@ -8,6 +11,66 @@ class RegistrationView(View):
         messages.get_messages(request)
         return render(request, "authentication/register.html")
     
+    def post(self, request):
+        data = request.POST
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+
+        context = {"fieldValues": request.POST}
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 8:
+                    messages.error(request, "Password should be 8 character long")
+                    return render(request, "authentication/register.html", context)
+                else:
+                    try:
+                        user = User.objects.create_user(username=username, email=email)
+                        user.set_password(password)
+                        user.save()
+                        transaction.commit()
+                        messages.success(request, "Account created")
+                    except Exception as e:
+                        messages.error(request, f"Server - Database warning {str(e)}")
+                        transaction.rollback()
+            else:
+                messages.error(request, "Email already exists")
+                return render(request, "authentication/register.html", context)
+        else:
+            messages.error(request, "User name already exists")
+            return render(request, "authentication/register.html", context)
+        
+        return redirect("login")
+    
 class UserLoginView(View):
     def get(self, request):
         return render(request, "authentication/login.html")
+    
+    def post(self, request):
+        data = request.POST
+        username = data.get("username")
+        password = data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, "Logged in successfully")
+                    return redirect("index")
+                else:
+                    messages.error(request, "Your account is not active")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Please provide both username and password.")
+        
+        return redirect("login")
+    
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, "Logout successfully")
+        return redirect("login")
